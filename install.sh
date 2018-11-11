73d3b7d3cb72197b65bf48be8119e3fe34878b22
BenjaminPMLovegood <benjaminpmlee@gmail.com>
2018-11-12 01:00:26 +0800

    add a missing chmod command

--- install.sh
+++ install.sh
@@ -15,6 +15,7 @@ installDocker() {
 
 installDotnet() {
     $GET https://dot.net/v1/dotnet-install.sh > ./dotnet-install.sh
+    chmod +x dotnet-install.sh
     ./dotnet-install.sh -v 2.1.403 -i $dir/$DOTNET_PATH
 }
 
